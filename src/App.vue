efd2206c2d887f9949202c357d0360cb034ffb20
eduardo239 <eucrieiumaconta@gmail.com>
2022-10-29 20:23:03 -0300

    base criada

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { auth } from './firebaseConfig';
 import { mapActions } from 'pinia';
-import { useChallenge } from '@/stores/challenges';
+import { useChallenge } from './stores/challenges';
 import { onMounted } from 'vue';
 import { NLayout } from 'naive-ui';
-import MainMenu from '@/components/menu/MainMenu';
+import MainMenu from './components/menu/MainMenu';
 
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
