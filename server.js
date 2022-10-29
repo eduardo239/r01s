3d049435f3f61e5231bb75cee9a0eef66a95f58c
eduardo239 <eucrieiumaconta@gmail.com>
2022-10-29 19:20:09 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import serveStatic from 'serve-static';
+import path from 'path';
+
+const app = express();
+const __dirname = process.cwd();
+console.log(__dirname);
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`app is listening on port: ${port}`);
