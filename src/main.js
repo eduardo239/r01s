82b1a2666c2fc28790cb80e0e357637489dee0d2
eduardo239 <eucrieumaconta@gmail.com>
2022-11-16 10:01:55 -0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import router from './router';
-import naive from 'naive-ui';
-import App from './App.vue';
-import './style.css';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+// import { createI18n } from "vue-i18n";
+import router from "./router";
+import naive from "naive-ui";
+import App from "./App.vue";
+import "./style.css";
+
+// const i18n = createI18n({});
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(naive).use(router).mount('#app');
+createApp(App).use(pinia).use(naive).use(router).mount("#app");
