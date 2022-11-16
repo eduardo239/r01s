ffebf8c08b0ad5dfd746d1089efe172b0a7c79e1
eduardo239 <eucrieumaconta@gmail.com>
2022-11-16 10:06:38 -0300

    adicionado dependencia i18n

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import router from "./router";
 import naive from "naive-ui";
 import App from "./App.vue";
 import "./style.css";
+import i18n from './i18n'
 
 // const i18n = createI18n({});
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(naive).use(router).mount("#app");
+createApp(App).use(i18n).use(pinia).use(naive).use(router).mount("#app");
